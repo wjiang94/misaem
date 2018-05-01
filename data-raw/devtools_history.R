ab0c766557db3058368d4667f2c78cca84b0456c
wjiang94 <wwjiangwei@hotmail.com>
2018-05-01 11:39:42 +0200

    Update devtools_history.R

--- data-raw/devtools_history.R
+++ data-raw/devtools_history.R
@@ -1,16 +1,8 @@
 devtools::use_data_raw()
 devtools::use_package("mice")
-<<<<<<< HEAD
 devtools::use_package("mvtnorm")
-=======
-<<<<<<< HEAD
-devtools::use_package("mvtnorm")
-=======
->>>>>>> 9c2a3444ff3ae2f5e0a70f129fa3a0b0fd9565a5
->>>>>>> 712a7782b605c46767c5e7e0f31f4f5f17523d32
 devtools::use_package("magrittr")
 devtools::use_vignette("misaem")
-
 devtools::build_vignettes()
 devtools::use_data(data.complete)
 devtools::use_data(X.complete)
