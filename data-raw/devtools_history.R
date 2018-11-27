ef5a440514b5c056ae920f9cb1ba23de650f6d54
Wei JIANG <wei.jiang@polytechnique.edu>
2018-11-27 01:27:25 +0100

    update

--- data-raw/devtools_history.R
+++ data-raw/devtools_history.R
@@ -1,6 +1,17 @@
 devtools::use_data_raw()
 devtools::use_package("mice")
 devtools::use_package("mvtnorm")
+
+#vignette
 devtools::use_package("magrittr")
 devtools::use_vignette("misaem")
 devtools::build_vignettes()
+
+#check problems
+devtools::check()
+
+#create a package bundle
+devtools::build()
+
+#check on windows and build a package bundle on windows
+devtools::build_win()
